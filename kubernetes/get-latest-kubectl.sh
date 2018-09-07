9989dbfc27d2a5d753532fbde0be18cf6e959f22
谢远东 <xieyuandong@xieyuandongdeMacBook-Pro.local>
2018-09-07 10:47:06 +0800

    get latest kubectl

---
+++ kubernetes/get-latest-kubectl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Finding latest kubectl..."
+# curl -s https://github.com/kubernetes/kubernetes/releases/latest  | awk -F '[<>]' '/.*/ { match($0, "tag/([^\"]+)",a); print a[1] }'
+LATEST=$(wget -qO- https://github.com/kubernetes/kubernetes/releases/latest | awk -F '[<>]' '/href="\/kubernetes\/kubernetes\/tree\/.*"/ { match($0, "tree/([^\"]+)",a); print a[1] }' | head -1)
+echo "Getting $LATEST..."
+sudo wget -NP /usr/bin http://storage.googleapis.com/kubernetes-release/release/$LATEST/bin/linux/amd64/kubectl
+sudo chmod 755 /usr/bin/kubectl
