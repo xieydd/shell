65ecb88f3f91ef36e7fac6609c0e41f14a1dcae3
xieydd <xieydd@gmail.com>
2018-09-12 10:54:51 +0800

    fix  it

--- kubernetes/NFS.sh
+++ kubernetes/NFS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 #未完成，这里需要修改/etc/yum.repos.d/Local.repo
-#并设定参数进行判定是nfs-service黑色nfs-client
+#并设定参数进行判定是nfs-service还是nfs-client
+#####Server#######
 yum -y install rpcbind nfs-utils
 
 systemctl start rpcbind
@@ -8,18 +9,19 @@ systemctl start nfs-server.service
 
 systemctl enable rpcbind nfs-utils
 
-#下面的IP可以修改
+#下面的IP根据nfs server的ip进行修改
 cat > /etc/exports << EOF
 /root/nfs 192.168.5.0/24(rw,sync,no_root_squash)
-
 EOF
-
+##################
+#####Client#######
 mkdir -p /root/nfs
 chmod 666 /root/nfs
 
 
-yum -y install  nfs-util
+yum -y install  nfs-utils
 
 mkdir -p /root/nfs
 chmod 666 /root/nfs
 mount  192.168.5.78:/root/nfs /root/nfs/
+#################
